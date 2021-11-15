9593f30a2382b13cf22a0573706ffa7a5e624c30
Nihar Maheshwari <nihar.maheshwari@columbia.edu>
2021-11-15 15:14:10 -0500

    for #15: Removing secret

--- src/constants/cognito_constants.py
+++ src/constants/cognito_constants.py
@@ -4,7 +4,7 @@ Constants for cognito
 USER_POOL_ID = 'us-east-2_iV4OqMaB3'
 CLIENT_ID = '89c4peguvnl94r51sgekgqiag'
 CLIENT_SECRET = 'kg7rdng88t07piqsdt1a3utjsoidcs6il6vs0ldadb6qrlfk0sq'
-ACCESS_KEY = 'AKIAVYPI7GZ6XNUZ4DFF'
-SECRET_ACCESS_KEY = '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'
+ACCESS_KEY = '<key>'
+SECRET_ACCESS_KEY = '<secret>'
 REGION = 'us-east-2'
 KEYS_URL = f"https://cognito-idp.{REGION}.amazonaws.com/{USER_POOL_ID}/.well-known/jwks.json"
