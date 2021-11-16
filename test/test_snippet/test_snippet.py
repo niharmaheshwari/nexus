22adbe50f8c25f8db1027f36436b597cde121e0b
Shantanu Jain <slj2142@columbia.edu>
2021-11-15 20:58:02 -0500

    Feature: Test Improvements Commit Message: Changing test cases for testsing updating of code snippets. Note: NA Jira-Ticket: #15

--- test/test_snippet/test_snippet.py
+++ test/test_snippet/test_snippet.py
@@ -18,7 +18,7 @@ class TestSnippetView(unittest.TestCase):
         self.stub_table = Stubber(self.snippet_manager.table.meta.client)
         self.stub_fs = Stubber(self.snippet_manager.fs)
         self.user = UserManager()
-        self.user.stubber = Stubber(self.user._cognito_client)
+        self.user.stubber = Stubber(self.user.cognito_client)
         self.stub_creds = Stubber(self.snippet_manager._creds)
 
     # Get flow
@@ -59,7 +59,7 @@ class TestSnippetView(unittest.TestCase):
             {"Attributes": {"id": {"S": "user1-snippit-1"}}}
         )
         self.stub_table.activate()
-        self.snippet_manager.user._cognito_client = MagicMock()
+        self.snippet_manager.user.cognito_client = MagicMock()
         self.snippet_manager.fs.upload_fileobj = MagicMock()
         self.snippet_manager.es.index = MagicMock()
         snippet = self.snippet_manager.create_snippet(data, file)
@@ -73,7 +73,7 @@ class TestSnippetView(unittest.TestCase):
         file = FileStorage(open('./test/test_snippet/binary_search_test.py', 'r'), 'abc.py')
         self.stub_table.add_client_error('put_item', 'ConnectionRefused')
         self.stub_table.activate()
-        self.snippet_manager.user._cognito_client = MagicMock()
+        self.snippet_manager.user.cognito_client = MagicMock()
         self.snippet_manager.fs.upload_fileobj = MagicMock()
         self.snippet_manager.es.index = MagicMock()
         failed = False
@@ -83,23 +83,6 @@ class TestSnippetView(unittest.TestCase):
             failed = True
         self.assertTrue(failed)
 
-    def test_update_snippet(self):
-        '''
-        Test creating of a new snippet
-        '''
-        data = open('./test/test_snippet/data.json', 'r')
-        file = FileStorage(open('./test/test_snippet/binary_search_test.py', 'r'), 'abc.py')
-        self.stub_table.add_response(
-            'put_item',
-            {"Attributes": {"id": {"S": "user1-snippit-1"}}}
-        )
-        self.stub_table.activate()
-        self.snippet_manager.user._cognito_client = MagicMock()
-        self.snippet_manager.fs.upload_fileobj = MagicMock()
-        self.snippet_manager.es.index = MagicMock()
-        snippet = self.snippet_manager.update_snippet(data, file)
-        self.assertIsNotNone(snippet)
-
     def test_update_snippet_fail(self):
         '''
         Test creating of a new snippet in the event of fail
@@ -108,7 +91,7 @@ class TestSnippetView(unittest.TestCase):
         file = FileStorage(open('./test/test_snippet/binary_search_test.py', 'r'), 'abc.py')
         self.stub_table.add_client_error('put_item', 'ConnectionRefused')
         self.stub_table.activate()
-        self.snippet_manager.user._cognito_client = MagicMock()
+        self.snippet_manager.user.cognito_client = MagicMock()
         self.snippet_manager.fs.upload_fileobj = MagicMock()
         self.snippet_manager.es.index = MagicMock()
         failed = False
