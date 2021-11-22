a139656f44fa5ac224025ac844a8975b9c249b56
Vaibhav Goyal <vaibhav.goyal@outlook.com>
2021-11-22 17:17:33 -0500

    Remove unused imports

--- src/client/src/modules/auth/router/index.tsx
+++ src/client/src/modules/auth/router/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from '../presentationLayer/login';
 import SignUp from '../presentationLayer/signUp';
 import NotFound from "../../home/presentationLayer/notFound";
