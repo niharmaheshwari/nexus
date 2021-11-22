0fe30062f3650b7988b75a185753212a6404d8f6
Vaibhav Goyal <vaibhav.goyal@outlook.com>
2021-11-22 17:13:22 -0500

    Update 404 page and home page

--- src/client/src/modules/home/presentationLayer/notFound/index.tsx
+++ src/client/src/modules/home/presentationLayer/notFound/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import {home} from "./style";
+import NexusCard from "../../../core/components/nexusCard";
 class NotFound extends React.Component {
   render(){
-    return <div> Page Not Found</div> 
+    return (
+        <div style={home}>
+          <NexusCard>
+            <h1> 404 Page Not Found</h1>
+          </NexusCard>
+        </div>
+    );
   }
 }
 
