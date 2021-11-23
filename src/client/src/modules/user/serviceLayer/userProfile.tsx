2885e1168b3643323ebcffbf5e560ec0c88d04a3
Vaibhav Goyal <vaibhav.goyal@outlook.com>
2021-11-22 21:10:04 -0500

    Add tokens to user profile

--- src/client/src/modules/user/serviceLayer/userProfile.tsx
+++ src/client/src/modules/user/serviceLayer/userProfile.tsx
@@ -2,8 +2,11 @@ class UserProfile {
     private static instance: UserProfile
     private name?: string
     private email?: string
+    private isAuthenticated: boolean
+    private token?: AuthTokens
 
     private constructor() {
+        this.isAuthenticated = false;
     }
 
     public static shared(): UserProfile {
@@ -13,9 +16,34 @@ class UserProfile {
         return UserProfile.instance;
     }
 
+    public updateAuthStatus(authenticated: boolean, token?: AuthTokens) {
+        this.isAuthenticated = authenticated;
+        this.token = token;
+    }
+
     public updateUserDetails(name?: string, email?: string) {
-        this.name = name
-        this.email = email
+        this.name = name;
+        this.email = email;
+    }
+}
+
+export class AuthTokens {
+    private accessToken?: string
+    private tokenExpiry?: number
+    private idToken?: string
+    private refreshToken?: string
+    private tokenType?: string
+
+    constructor(accessToken: string,
+                expiry: number,
+                idToken: string,
+                refreshToken: string,
+                tokenType: string) {
+        this.accessToken = accessToken;
+        this.tokenExpiry = expiry
+        this.idToken = idToken
+        this.refreshToken = refreshToken
+        this.tokenType = tokenType
     }
 }
 
