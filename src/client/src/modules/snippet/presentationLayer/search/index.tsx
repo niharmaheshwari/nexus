2134acb441741c989085f61acc50d216829f6870
Vaibhav Goyal <vaibhav.goyal@outlook.com>
2021-11-23 17:18:34 -0500

    Create basic search layout

--- src/client/src/modules/snippet/presentationLayer/search/index.tsx
+++ src/client/src/modules/snippet/presentationLayer/search/index.tsx
@@ -1,12 +1,60 @@
 import React from 'react'
-import {home} from "../../../home/presentationLayer/landing/style";
+import {card} from "./style";
 import NexusCard from "../../../core/components/nexusCard";
-class SnippetSearch extends React.Component {
+import {Button, Grid, TextField} from "@mui/material";
+
+interface Props {
+    [name: string]: any
+}
+
+interface State {
+    searchQuery: string
+    [name: string]: any
+}
+
+class SnippetSearch extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            searchQuery: "",
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event: React.SyntheticEvent) {
+        const { name, value } = event.target as HTMLButtonElement;
+        this.setState({
+            ...this.state,
+            [name]: value,
+        });
+    }
+
     render(){
         return (
-            <div style={home}>
+            <div style={card}>
                 <NexusCard>
                     <h1> Snippet Search </h1>
+                    <Grid style={{paddingTop: "10px", paddingBottom: "10px"}} container alignItems="center" direction="row" justifyContent="flex-start" spacing={2}>
+                        <Grid item>
+                            <TextField
+                                id="search-input"
+                                name="search"
+                                label="Search"
+                                type="text"
+                                size="small"
+                                value={this.state.searchQuery}
+                                onChange={this.handleChange}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Button variant="contained" color="primary">
+                                Search
+                            </Button>
+                        </Grid>
+                    </Grid>
                 </NexusCard>
             </div>
         );
