03e30302b38efd866ba15b8ae354b9c4a916e844
Vaibhav Goyal <vaibhav.goyal@outlook.com>
2021-11-22 19:34:47 -0500

    Create user profile

---
+++ src/client/src/modules/user/serviceLayer/userProfile.tsx
@@ -0,0 +1,20 @@
+class UserProfile {
+    private static instance: UserProfile
+    private name?: string
+    private email?: string
+
+    private constructor() {
+    }
+
+    public static shared(): UserProfile {
+        if (!UserProfile.instance) {
+            UserProfile.instance = new UserProfile();
+        }
+        return UserProfile.instance;
+    }
+
+    public updateUserDetails(name?: string, email?: string) {
+        this.name = name
+        this.email = email
+    }
+}
