112df578a608b35f93f97e29dde43e9de510863e
Vaibhav Goyal <vaibhav.goyal@outlook.com>
2021-11-25 00:24:21 -0500

    Add date formatting

--- src/client/src/modules/auth/serviceLayer/signUpService.ts
+++ src/client/src/modules/auth/serviceLayer/signUpService.ts
@@ -4,12 +4,24 @@ class SignUpService {
     constructor() {
         console.log("Sign Up Service Created")
     }
+    getFormattedDate(date: Date): string {
+        const year = date.getFullYear();
+
+        let month = (1 + date.getMonth()).toString();
+        month = month.length > 1 ? month : '0' + month;
+
+        let day = date.getDate().toString();
+        day = day.length > 1 ? day : '0' + day;
+
+        return month + '/' + day + '/' + year;
+    }
     async signUp(name: string,
                  email: string,
                  phone: string,
                  birthdate: Date,
                  password: string): Promise<any> {
-        const requestParams = new SignUpRequestParams(name, email, phone, password, birthdate.toDateString());
+        const birthdayString = this.getFormattedDate(birthdate);
+        const requestParams = new SignUpRequestParams(name, email, phone, password, birthdayString);
         const response = await NetworkLayer.apiRequest("post",
             "auth/signup",
             undefined,
