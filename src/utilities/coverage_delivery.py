714c720a69305ab6307839aedbec48f5ce52f0ba
Nihar Maheshwari <nihar.maheshwari@columbia.edu>
2021-11-27 18:00:45 -0500

    For #40 : Enabling variable fetch for coverage delivery

--- src/utilities/coverage_delivery.py
+++ src/utilities/coverage_delivery.py
@@ -4,7 +4,12 @@ Helper routine for writing the coverage to S3
 import boto3
 import os
 
-fs = boto3.client('s3')
+session = boto3.Session(
+    aws_access_key_id=os.environ['ACCESS_KEY'],
+    aws_secret_access_key=os.environ['SECRET_ACCESS_KEY'],
+)
+fs = session.client('s3')
+
 for root, dirs, files in os.walk('test/coverage', topdown=False):
     for name in files:
         fs.upload_file(
