e8b3967a0a16e620939f55d0f82a619ec6b20b85
Nihar Maheshwari <nihar.maheshwari@columbia.edu>
2021-11-27 17:03:55 -0500

    For #40 - Adding secrets file with params

---
+++ src/constants/secrets.py
@@ -0,0 +1,9 @@
+import os
+
+USER_POOL_ID=os.environ['USER_POOL_ID']
+CLIENT_ID=os.environ['CLIENT_ID']
+CLIENT_SECRET=os.environ['CLIENT_SECRET']
+ACCESS_KEY=os.environ['ACCESS_KEY']
+SECRET_ACCESS_KEY=os.environ['SECRET_ACCESS_KEY']
+REGION=os.environ['REGION']
+KEYS_URL=os.environ['KEYS_URL']
