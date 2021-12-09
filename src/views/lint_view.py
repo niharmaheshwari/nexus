0019b72307876b2cb64f954e156d420e171955f3
tykosc <talyakosch@gmail.com>
2021-12-09 16:02:18 -0500

    logger to lint

--- src/views/lint_view.py
+++ src/views/lint_view.py
@@ -1,14 +1,17 @@
 '''lint View'''
+import logging
 from src.manager.linting import linting_manager
 from flask import (Blueprint, request, Response, jsonify)
 from src.manager.snippet_manager import SnippetManager
 from src.utilities.authorization import authorization
+from src.utilities.logging import logger
 
 lint = Blueprint('lint', __name__, url_prefix='/api/lint')
 manager = SnippetManager()
 
 @lint.route('/<snippet_id>', methods=['GET'])
 @authorization
+@logger
 def get_lint_output(snippet_id):
     '''
     Given a snippet ID, run a linter, return the output
